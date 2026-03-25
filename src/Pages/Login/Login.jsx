67e686846e614d55acd277f029c56cd8fa853464
AmstrongBaskaran <aamstrong522@gmail.com>
2026-03-25 12:12:31 +0530

    intial commit

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,25 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!email){
+      setError("Email is required")
+      return
+    }
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)){
+      setError("Enter a Valid Email");
+      return
+    }
+    if (!password){
+      setError("Enter a password");
+      return
+    }
+    if (password.length < 6){
+      setError("Password must be at least 6 characters");
+      return
+    }
+    setError("");
+    try{
+      const res = await fetch("",{
+        method:"post",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({email,password})
+      });
+      const data = await res.json
+      if(!res){
+        setError(data.message);
+      }else{
+        alert("login successfull")
+      }
+    }
+     catch(err){
+      setError("Server Error")
+     }
+
+  }
   return (
     <div className="login-page">
       <div className="login-box">
         <h1>Eventify</h1>
         <h2>Welcome Back</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Enter your email" />
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+            
+          />
+
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
             placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+            
           />
+          {error && <p className="error" style={{color:"red"}}>{error}</p>}
+
           <button type="submit">Login</button>
         </form>
-        <p className="signup-link">
-        </p>
+
       </div>
     </div>
   );
